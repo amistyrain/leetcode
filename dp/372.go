32d8ec5917b9b71f0d1b3447d5c867b6029627e4
sjw <webmrsong@gmail.com>
2021-07-05 21:03:28 +0800

    fix

--- dp/372.go
+++ dp/372.go
@@ -3,28 +3,55 @@ package main
 import "fmt"
 
 // 双指针
-func isSubsequence(s string, t string) bool {
-	if len(s) == 0 {
-		return true
-	}
-	i := len(s) - 1
-	j := len(t) - 1
+// func isSubsequence(s string, t string) bool {
+// 	if len(s) == 0 {
+// 		return true
+// 	}
+// 	i := len(s) - 1
+// 	j := len(t) - 1
 
-	for j >= 0 {
-		if s[i] == t[j] {
-			if i == 0 {
-				return true
-			}
-			j--
-			i--
-			continue
-		}
-		j--
-	}
+// 	for j >= 0 {
+// 		if s[i] == t[j] {
+// 			if i == 0 {
+// 				return true
+// 			}
+// 			j--
+// 			i--
+// 			continue
+// 		}
+// 		j--
+// 	}
 
-	return false
-}
+// 	return false
+// }
 
 func main() {
-	fmt.Println(isSubsequence(`b`, `abc`))
+	fmt.Println(isSubsequence(`bb`, `ab`))
+}
+
+func isSubsequence(s string, t string) bool {
+	dp := make([][]int, len(s)+1)
+	for i, _ := range dp {
+		dp[i] = make([]int, len(t)+1)
+	}
+
+	for i := 1; i < len(s)+1; i++ {
+		for j := 1; j < len(t)+1; j++ {
+			if s[i-1] == t[j-1] {
+				dp[i][j] = dp[i-1][j-1] + 1
+			} else {
+				dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+			}
+		}
+	}
+
+	return dp[len(s)][len(t)] == len(s)
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+
+	return b
 }
